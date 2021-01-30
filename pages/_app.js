8c4bd3edf5b9940579b1661ff2d402936ab1722b
Cristovão Jobs <minguibernardo@gmail.com>
2021-01-30 11:34:47 +0100

    new change

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
+import { ProvideAuth } from '../lib/auth'
 import '../styles/globals.css'
 
+{/*Inicializado a app */}
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return  <ProvideAuth>
+           <Component {...pageProps}/>
+        </ProvideAuth> // criando provedor do context para todas paginas que fore, gerado
+     }
 
 export default MyApp
