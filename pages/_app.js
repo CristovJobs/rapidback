06349d844adfaf13135a3a23ce056fd21bd20a89
Cristovão Jobs <minguibernardo@gmail.com>
2021-01-31 00:21:17 +0100

    login-ui progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,48 @@
-import { ProvideAuth } from '../lib/auth'
-import '../styles/globals.css'
+import { AuthProvider } from '@lib/auth';
+import { ThemeProvider, CSSReset } from '@chakra-ui/react';
+import customTheme from '@styles/theme';
+import { Global, css } from '@emotion/react';
 
-{/*Inicializado a app */}
+
+const GlobaListyle = ({ children }) => {
+  return (
+
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+
+          @import url('https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500&display=swap');
+
+          html {
+                 min-width:360px;
+                 scroll-bahavior:smooth;
+                 font-family: 'Baloo 2', cursive !important;
+
+           }
+
+          #__next {
+                     display:flex;
+                     flex-direction: column;
+                     min-height:100vh;
+             }
+          `}
+         />
+      {children}
+    </>
+  );
+};
+
+
+{/*Inicializado a app */ }
 function MyApp({ Component, pageProps }) {
-  return  <ProvideAuth>
-           <Component {...pageProps}/>
-        </ProvideAuth> // criando provedor do context para todas paginas que fore, gerado
-     }
+  return <ThemeProvider theme={customTheme}> {/**tema do aplicativo */}
+    <AuthProvider>
+      <GlobaListyle />
+      <Component {...pageProps} />
+
+    </AuthProvider>
+  </ThemeProvider> // criando provedor do context para todas paginas que fore, gerado
+}
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,40 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import {useAuth} from "../lib/auth";
+import { useAuth } from "@lib/auth";
+import { Box, Button, Code, Flex, Heading, Text } from "@chakra-ui/react"
+import { ChatIcon } from '@chakra-ui/icons'
 
-const  Home = () => {
+
+const Home = () => {
 
   const auth = useAuth();
-  return <div className={styles.container}>
-  <Head>
-    <title>Create Next App</title>
-    <link rel="icon" href="/favicon.ico" />
-  </Head>
+  return <Flex as="main" direction="column" align="center" justify="center" maxW="100%" h="100vh">
+    <Head>
+      <title>RapidBack</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-  <main className={styles.main}>
-    <h1 className={styles.title}>
-     RapidBack for life
-    </h1>
-     <p>
-    <small><b>{auth?.user?.displayName}</b></small>
-    </p>
 
-    <button onClick={(e) => auth.signWithGithub()}>signIn With Github</button>  {/* logar na sessao */}
 
-    {auth?.user && (
-       <button onClick={(e) => auth.signout()}>Sign Out</button> 
-    )} {/* deslogar da sessao */}
-   
-</main>
-</div>
+    <ChatIcon w={40} h={40} color="blue.500" />
+
+    <Heading  fontFamily="" fontSize="40px" mt={4}>RapidBack</Heading>
+
+    <Text>
+
+      <Code mt={4}>Fest and sample feedback for you</Code>
+
+    </Text>
+
+
+    {/* deslogar da sessao */}
+    {auth.user ? (
+      <Button mt={4} onClick={(e) => auth.signout()}>Sign Out</Button>
+    ) : (
+        <Button mt={4} colorScheme="blue"  variant="outline" size="sm" onClick={(e) => auth.signWithGithub()}>SignIn With Github</Button>
+      )}
+    {/* logar na sessao */}
+
+  </Flex>
 };
 
 
